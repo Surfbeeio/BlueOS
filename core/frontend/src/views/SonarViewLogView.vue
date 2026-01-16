ea9bde70bb3b10d05a52315c5e0e7f887ea8160c
jhacksurf <140029930+jhacksurf@users.noreply.github.com>
2026-01-16 14:47:48 +1100

    Use file browser for SonarView logs

--- core/frontend/src/views/SonarViewLogView.vue
+++ core/frontend/src/views/SonarViewLogView.vue
@@ -1,16 +1,28 @@
 <template>
-  <SonarViewLogManager />
+  <BrIframe
+    :source="full_path"
+  />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-import SonarViewLogManager from '@/components/logs/SonarViewLogManager.vue'
+import BrIframe from '@/components/utils/BrIframe.vue'
 
 export default Vue.extend({
   name: 'SonarViewLogView',
   components: {
-    SonarViewLogManager,
+    BrIframe,
+  },
+  data() {
+    return {
+      service_path: '/file-browser/#/files/userdata/sonarview',
+    }
+  },
+  computed: {
+    full_path(): string {
+      return this.service_path
+    },
   },
 })
 </script>
